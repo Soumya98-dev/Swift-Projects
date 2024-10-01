dab43349cdafe95be65e101a1f5fbe35efb99a8c
Soumyadeep Chatterjee <soumyadeepchatterjee@Soumyadeeps-MacBook-Pro-2.local>
2024-09-30 21:40:15 -0400

    Initial commit

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Soumyadeep Chatterjee on 9/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+        Form{
+            Section{
+                Text("Hello, world")
+            }
+            Section{
+                Text("Hello, world")
+                Text("Hello, world")
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Soumyadeep Chatterjee on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
